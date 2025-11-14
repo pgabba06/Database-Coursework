6f101ed5f6a5bda01243de52b7b2b79ae06cc006
pgabba06 <153506645+pgabba06@users.noreply.github.com>
2025-11-14 15:45:40 +0000

    Add files via upload

---
+++ query4.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE VIEW query4 AS
+SELECT 
+c.name AS customer_name,
+i.name AS item_name,
+MAX(r.stars) AS highest_rating,
+MIN(r.stars) AS lowest_rating
+FROM rating r
+JOIN customer c ON r.customer_id = c.id
+JOIN item i ON r.item_id = i.id
+GROUP BY c.name, i.name
+HAVING COUNT(*) > 1
+ORDER BY c.name ASC;
