b1bef64fe5312973f461eb52bfe0295bab0ef3b4
pgabba06 <153506645+pgabba06@users.noreply.github.com>
2025-11-17 14:12:14 +0000

    Delete query3.sql

--- query3.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE VIEW query3 AS
-SELECT DISTINCT i.name AS item_name,
-MAX(r.date_time) AS most_recent_rating,
-ROUND(AVG(r.stars), 2) AS average_stars
-FROM item i
-JOIN rating r ON i.id = r.item_id
-GROUP BY i.name
-ORDER BY average_stars ASC;
